9ecc4b7ef49ee5cfec11b6eeaa56365d24c9aba1
novakGregor <gregor.novak11@gmail.com>
2023-01-18 00:39:42 +0100

    Try with @OpenAPIDefinition

--- api/src/main/java/rso/itemscompare/entityeditor/api/v1/EntityEditorApplication.java
+++ api/src/main/java/rso/itemscompare/entityeditor/api/v1/EntityEditorApplication.java
@@ -1,8 +1,14 @@
 package rso.itemscompare.entityeditor.api.v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/v1")
+@OpenAPIDefinition(info = @Info(title = "EntityEditor", version = "v1"),
+        servers = @Server(url = "http://20.31.253.184/entity-editor"))
 public class EntityEditorApplication extends Application {
 }
